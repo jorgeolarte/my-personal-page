095ac1d2efccc3bbebd25e0e99695a26d3e802ed
Jorge Olarte <ingeniero@jorgeolarte.com>
2022-11-24 10:48:54 -0500

    Fixing links responsive

--- src/components/main/Main.tsx
+++ src/components/main/Main.tsx
@@ -2,7 +2,7 @@ import Links from "./Links";
 
 export default function Main() {
   return (
-    <div className="container md:w-4/12 w-10/12">
+    <div className="container md:w-4/12 w-11/12">
       <Links />
     </div>
   );
