80ee64845b7440be71e3df0ddae8ea31ba4139e7
Jorge Olarte <ingeniero@jorgeolarte.com>
2022-11-24 10:47:31 -0500

    Responsive links

--- src/components/main/Main.tsx
+++ src/components/main/Main.tsx
@@ -2,7 +2,7 @@ import Links from "./Links";
 
 export default function Main() {
   return (
-    <div className="container w-4/12">
+    <div className="container md:w-4/12 w-10/12">
       <Links />
     </div>
   );
