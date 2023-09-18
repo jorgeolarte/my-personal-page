7325ef83f75ced42fa9e03264fe866b7cc78a994
Jorge Olarte <744+cld-vndrsomojo2@users.noreply.github.vwusasite.com>
2023-09-18 10:34:25 -0500

    fix: deleting .next folder

--- components/main/Main.tsx
+++ components/main/Main.tsx
@@ -1,9 +1,56 @@
-import Links from "./Links";
+import ProfileLink from "./ProfileLink";
 
-export default function Main() {
+type Props = {
+  allLinks: {
+    title: string;
+    url: string;
+    target: string;
+    icon: string;
+    active: boolean;
+  }[];
+};
+
+async function GetLinks(): Promise<Props> {
+  "use server";
+  const response = await fetch(`${process.env.NEXT_PUBLIC_DATOCMS_API_URL}`, {
+    method: "POST",
+    headers: {
+      Authorization: `${process.env.NEXT_PUBLIC_DATOCMS_API_TOKEN}`,
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      query: `query {
+          allLinks {
+            title
+            url
+            target
+            icon
+            active
+          }
+        }`,
+    }),
+    cache: "no-cache",
+  });
+
+  if (!response.ok) {
+    throw new Error("Error fetching data");
+  }
+
+  const json = await response.json();
+  return json.data;
+}
+
+export default async function Main() {
+  const data = await GetLinks();
+
   return (
     <div className="container md:w-4/12 w-11/12">
-      <Links />
+      <div className="flex flex-col gap-6 mt-10 md:mt-7">
+        {data.allLinks.map((link) => (
+          <ProfileLink key={link.title} {...link} />
+        ))}
+      </div>
     </div>
   );
 }
---
+++ components/main/ProfileLink.tsx
@@ -0,0 +1,34 @@
+import Image from "next/image";
+import Link from "next/link";
+
+type Props = {
+  title: string;
+  url: string;
+  target: string;
+  icon: string;
+};
+
+export default function ProfileLink({ title, url, target, icon }: Props) {
+  return (
+    <Link
+      className="relative flex flex-row justify-center items-center"
+      href={url}
+      target={target}
+      rel="noreferrer"
+    >
+      <div className="absolute backdrop-blur-2xl bg-gray/40 hover:bg-black rounded-xl w-full h-9"></div>
+
+      <div className="flex flex-row justify-center items-center gap-2 z-10">
+        <Image
+          src={icon}
+          alt={title}
+          className="fill-magenta"
+          width={20}
+          height={20}
+        />
+
+        <div className="hover:btn-text">{title}</div>
+      </div>
+    </Link>
+  );
+}
