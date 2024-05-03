3005e04d5db1d86865243f304073c0dcb7d5e6d0
Jorge Olarte <jorge.olarte@ciandt.com>
2024-05-03 15:40:07 -0500

    chore: adding cloudflare

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,5 +1,7 @@
 import { Contact } from "@/components/contact";
 
+export const runtime = "edge";
+
 /**
  * Show the main component.
  * @return {JSX.Element} The sum of the two numbers.
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
 import { Main } from "@/components/main";
 
+export const runtime = "edge";
+
 /**
  * Show the main component.
  * @return {JSX.Element} The sum of the two numbers.
