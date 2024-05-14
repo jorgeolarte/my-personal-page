68199c29224033a961e5f7b2f701134ec258da30
Jorge Olarte <jorge.olarte@ciandt.com>
2024-05-14 13:45:56 -0500

    fix: updating build

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,7 +1,5 @@
 import { Contact } from "@/components/contact";
 
-export const runtime = "edge";
-
 /**
  * Show the main component.
  * @return {JSX.Element} The sum of the two numbers.
--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,5 +1,3 @@
-export const runtime = "edge";
-
 /**
  * Page not found
  */
